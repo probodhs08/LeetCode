fe891137347e7b8621f40caab90a990da7b43dbd
Probodhsahoo12220565 <119026367+probodhs08@users.noreply.github.com>
2025-02-26 18:33:34 +0530

    Time: 2 ms (77.68%), Space: 13.5 MB (10.17%) - LeetHub

--- 0013-roman-to-integer/0013-roman-to-integer.cpp
+++ 0013-roman-to-integer/0013-roman-to-integer.cpp
@@ -1,36 +1,16 @@
 class Solution {
 public:
     int romanToInt(string s) {
-    int sum=0;
-    for(int i=0;i<s.length();i++){
-        if(s[i]=='I' && (s[i+1]=='V' || s[i+1]=='X')){
-        sum-=1;
-        }else if(s[i]=='X' && (s[i+1]=='L' || s[i+1]=='C')){
-        sum-=10;
-        }else if(s[i]=='C' && (s[i+1]=='D' || s[i+1]=='M')){
-        sum-=100;
-        }
-        else{ 
-        if(s[i]=='I'){
-            sum+=1;
-        }else if(s[i]=='V'){
-            sum+=5;
-        }else if(s[i]=='X'){
-            sum+=10;
-        }else if(s[i]=='L'){
-            sum+=50;
-        }else if(s[i]=='C'){
-            sum+=100;
-        }else if(s[i]=='D'){
-            sum+=500;
-        
-        }else if(s[i]=='M'){
-            sum+=1000;
+      unordered_map<char, int>map={{'I',1},{'V',5},{'X',10},{'L',50},{'C',100},{'D',500},{'M',1000}};
+      int sum=0;
+      int n=s.size();
+      for(int i=0;i<n;i++){
+        if(i<n && map[s[i]]<map[s[i+1]]){
+            sum-=map[s[i]];
         }else{
-            sum+=0;
+            sum+=map[s[i]];
         }
-        }
-    }   
-    return sum; 
+      }
+      return sum;  
     }
 };
